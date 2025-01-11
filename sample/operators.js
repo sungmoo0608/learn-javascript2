046626091bfb0b495af04ad1f13a134960ed2c53
SWINGSM89\hegyo <swingsm0608@gmail.com>
2025-01-11 22:28:06 +0900

    Javascript2-learn-4-operators

---
+++ sample/operators.js
@@ -0,0 +1,27 @@
+'use strict';
+
+console.log(3 + 4);
+console.log(3 - 4);
+console.log(3 * 4);
+console.log(3 / 4);
+console.log(3 % 4);
+
+console.log('==================');
+
+let result = 1 + 1 / 2 + 2 * 3 + 3 + 3;
+console.log(result);
+result = 1 - 1;
+console.log(result);
+result = 1 * 1;
+console.log(result);
+result = 1 / 2;
+console.log(result);
+
+console.log('==================');
+
+//복합대입연산자
+let result1 = 3;
+result1 += 3; //result+3
+console.log(result1);
+result1 -= 2; //result-2
+console.log(result1);
