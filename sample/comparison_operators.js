4383d158eab3cab84f4f48390aa084c1c1b3c467
SWINGSM89\hegyo <swingsm0608@gmail.com>
2025-01-11 23:14:01 +0900

    Javascript2-learn-7-Comparison-operators

---
+++ sample/comparison_operators.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let num1 = 3;
+let num2 = 5;
+
+console.log(num1 > num2);
+console.log(num1 < num2);
+
+console.log('============================');
+
+let num3 = '3';
+let num4 = 3;
+
+console.log(num3 == num4);
+console.log(num3 === num4);
+console.log(num3 != num4);
+console.log(num3 !== num4);
