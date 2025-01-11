47e720bec3eb2afed6c40c179a7b1018e4b2a1b6
SWINGSM89\hegyo <swingsm0608@gmail.com>
2025-01-11 23:28:27 +0900

    Javascript2-learn-8-if-else

---
+++ sample/if_else.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let number = 3;
+
+if (number == 5) {
+  console.log('조건으로 사용된 비교연산식이 참을 반환했다!');
+  console.log('조건으로 사용된 비교연산식이 참을 반환했다!');
+  console.log('조건으로 사용된 비교연산식이 참을 반환했다!');
+  console.log('조건으로 사용된 비교연산식이 참을 반환했다!');
+  console.log('조건으로 사용된 비교연산식이 참을 반환했다!');
+  console.log('조건으로 사용된 비교연산식이 참을 반환했다!');
+  console.log('조건으로 사용된 비교연산식이 참을 반환했다!');
+} else {
+  console.log('거!짓!말!');
+  console.log('거!짓!말!');
+  console.log('거!짓!말!');
+  console.log('거!짓!말!');
+  console.log('거!짓!말!');
+}
+
+console.log('독립적으로 사용된 콘솔로그');
+document.querySelector('none');
+let hello = 'hello';
+console.log(hello);
