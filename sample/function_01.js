80f72dc5f734c1ec11e1b20359a04f1e60cdb435
SWINGSM89\hegyo <swingsm0608@gmail.com>
2025-01-12 00:07:07 +0900

    Javascript2-learn-11-function_01

---
+++ sample/function_01.js
@@ -0,0 +1,20 @@
+'use strict';
+
+//함수 선언식
+function sayHello() {
+  let hello = '문자열 헬로';
+  console.log('Hello');
+  console.log('hi');
+  console.log('안녕하시렵니까?');
+  console.log(hello);
+}
+sayHello();
+
+console.log('=======================');
+
+// 함수 표현식
+const sayBye = function () {
+  console.log('bye');
+};
+sayBye();
+console.log(hello);
