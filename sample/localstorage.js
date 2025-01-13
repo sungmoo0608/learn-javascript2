22da59b6b07fe95ae4d75a279b6d5ffe923f3958
sungmoo0608 <swingsm0608@gmail.com>
2025-01-13 11:37:53 +0900

    Javascript2-learn-20-localStorage

---
+++ sample/localstorage.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// const myName = '바밤바';
+
+// console.log(myName);
+
+// localStorage.setItem('myName', myName);
+
+// const myName = localStorage.getItem('myName');
+// alert(myName);
+
+localStorage.setItem('cat', '고양이');
+localStorage.setItem('dog', '강아지');
+localStorage.setItem('myName', '수박바');
+
+localStorage.removeItem('cat');
+localStorage.clear();
